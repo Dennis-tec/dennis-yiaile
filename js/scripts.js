8d84ea0cf395e3ed89b6777fdc2987491f8654d5
Dennis Yiaile <ykamakei6345@gmail.com>
2021-06-09 21:27:29 +0300

    add java script information

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$("document").ready(function(){
+  $("#button").click(function(){
+    var name=$("#name").val();
+    var email=$("#email").val();
+    var message=$("#message").val();
+    if (!name && !email) {
+    alert("Enter your contact details!")
+  }else if (!email) {
+    alert(name +", enter your email!");
+  } else if (!name) {
+    alert("Enter your name!")
+  } else {
+      alert(name+ ", I have receive your information. I will get back to you in a few!");
+  }
+  })
+  })
