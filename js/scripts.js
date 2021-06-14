0a6178d8e851ba1ef7ee5079409e707dd8c8d6d1
Dennis Yiaile <ykamakei6345@gmail.com>
2021-06-14 22:12:08 +0300

    remove input texts after alert

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,16 @@ $("document").ready(function(){
     var email=$("#email").val();
     var message=$("#message").val();
     if (!name && !email) {
-    alert("Enter your contact details!")
+    alert("Enter your contact details!");
+    $("#message").val("");
+    return false;
   }else if (!email) {
     alert(name +", enter your email!");
+    return false;
   } else if (!name) {
     alert("Enter your name!")
-  } else {
+    return false;
+  }else {
       alert(name+ ", Your details have been send to Dennis Yiaile. In case of any questions or concerns, I will get back to you ASAP!");
   }
   })
