49b04496427cd3ae4162a8ff83d3f14a257bba6a
Dennis Yiaile <ykamakei6345@gmail.com>
2021-06-17 00:11:52 +0300

    add new email link

--- js/scripts.js
+++ js/scripts.js
@@ -16,18 +16,25 @@ $("document").ready(function(){
     return false;
   }else {
     confirm(name + ", your details will be send to Dennis Yiaile.");
+    alert(name+ ", your information have been send to Dennis Yiaile. In case of any questions or concerns, I will get back to you ASAP!");
       }
   })
   })
   function sendMail() {
-Email.send({
-SecureToken : "dfa0c644-fb01-4096-865a-368f825535e3",
-To : 'ykamakei6345@gmail.com',
-From : "ykamakei6345@gmail.com",
-Subject: "Sending Email using javascript with SMTPJS",
-Body: document.getElementById('message').value,
-})
-.then(function (message) {
-  alert("Your information have been send to Dennis Yiaile. In case of any questions or concerns, I will get back to you ASAP!");
-});
-}
+    var link = "mailto:denno6345@gmail.com"
+             + "?cc=denno6345@gmail.com"
+             + "&subject=" + encodeURIComponent("This is my subject")
+             + "&body=" + encodeURIComponent(document.getElementById('message').value);
+
+    window.location.href = link;
+//Email.send({
+//SecureToken : "dfa0c644-fb01-4096-865a-368f825535e3",
+//To : 'ykamakei6345@gmail.com',
+//From : "ykamakei6345@gmail.com",
+//Subject: "Sending Email using javascript with SMTPJS",
+//Body: document.getElementById('message').value,
+//})
+//.then(function (message) {
+//alert("Mail has been sent successfully")
+//});
+//}
